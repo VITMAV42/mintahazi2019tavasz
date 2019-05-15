41942949ce4a7380b0962d73974215b28829271e
Zsombor Paróczi <zsombor.paroczi@gmail.com>
2019-05-15 20:22:48 +0200

    Toplist MW

--- middleware/nagymama/getTopNagymamakMW.js
+++ middleware/nagymama/getTopNagymamakMW.js
@@ -5,18 +5,46 @@
 const requireOption = require('../requireOption');
 
 module.exports = function(objectrepository) {
+    const BefottModel = requireOption(objectrepository, 'BefottModel');
     return function(req, res, next) {
-        // TODO: get data from DB
-        res.locals.toplista = [
-            {
-                nev: 'Marika',
-                befottszam: 15
-            },
-            {
-                nev: 'Terike',
-                befottszam: 11
+        // maaaagic....
+        BefottModel.aggregate(
+            [
+                {
+                    $group: {
+                        _id: '$_befozo',
+                        count: {
+                            $sum: 1
+                        }
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'nagymamas',
+                        localField: '_id',
+                        foreignField: '_id',
+                        as: 'befozo'
+                    }
+                },
+                {
+                    $sort: {
+                        count: -1
+                    }
+                },
+                {
+                    $limit: 10
+                },
+                { $unwind: { path: '$befozo' } }
+            ],
+            function(err, result) {
+                if (err) {
+                    return next(err);
+                }
+                res.locals.toplista = result.map(e => {
+                    return { nev: e.befozo.nev, befottszam: e.count };
+                });
+                return next();
             }
-        ];
-        return next();
+        );
     };
 };
